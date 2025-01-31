e474fffd041228a95cc312b794cf080f34eae342
Mitchelle <mitchellekaran@gmail.com>
2025-01-30 21:41:35 -0800

    Added functionality to add and display multiple quotes

--- random_quote.py
+++ random_quote.py
@@ -15,3 +15,34 @@ if __name__ == "__main__":
     print("Random Quote Generator")
     print("----------------------")
     print(get_random_quote())
+
+def display_multiple_quotes(n):
+    if n > len(quotes):
+        print("Not enough quotes available.")
+        return
+    for quote in random.sample(quotes, n):
+        print(f'- {quote}')
+
+if __name__ == "__main__":
+    while True:
+        print("\nOptions:")
+        print("1. Get a Random Quote")
+        print("2. Add a New Quote")
+        print("3. Display Multiple Quotes")
+        print("4. Exit")
+        
+        choice = input("Enter your choice: ")
+        
+        if choice == "1":
+            print(get_random_quote())
+        elif choice == "2":
+            new_quote = input("Enter the new quote: ")
+            add_quote(new_quote)
+        elif choice == "3":
+            n = int(input("Enter number of quotes to display: "))
+            display_multiple_quotes(n)
+        elif choice == "4":
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
